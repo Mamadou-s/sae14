fda43adfe6f5b268aa741519ad23f2a40a6a01c0
Mamadou -s <bahsadialiou2001@gmail.com>
2023-01-22 15:24:43 +0100

    s

--- src/Controller/BahController.php
+++ src/Controller/BahController.php
@@ -17,4 +17,42 @@ class BahController extends AbstractController
             'controller_name' => 'BahController',
         ]);
     }
+
+     /**
+     * @Route("/cv", name="cv")
+     */
+    public function page2(): Response
+    {
+        return $this->render('bah/cv.html.twig', [
+            'controller_name' => 'BahController',
+        ]);
+    }
+     /**
+     * @Route("/loisir", name="loisir")
+     */
+    public function page3(): Response
+    {
+        return $this->render('bah/loisir.html.twig', [
+            'controller_name' => 'BahController',
+        ]);
+    }
+     /**
+     * @Route("/portfolio", name="portfolio")
+     */
+    public function page4(): Response
+    {
+        return $this->render('bah/portfolio.html.twig', [
+            'controller_name' => 'BahController',
+        ]);
+    }
+     /**
+     * @Route("/contact", name="contact")
+     */
+    public function page5(): Response
+    {
+        return $this->render('bah/contact.html.twig', [
+            'controller_name' => 'BahController',
+        ]);
+    }
 }
+
